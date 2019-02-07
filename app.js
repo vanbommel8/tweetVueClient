004d5e75a261483b45e479f37058215e45185fbc
vanbommel8 <vanbommel8@gmail.com>
2019-02-07 15:03:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var vue = new Vue({
+    el: '#app',
+    data: {
+        tweets: [],
+        tweet:"",
+        author:"",
+        message: "",
+        idTweets: ""
+    },
+
+
+    methods: {
+
+        addTweets: function() {
+            this.$http.post('https://fake-tweets-api.herokuapp.com/tweets',{
+                author:this.author,
+                message:this.message,
+            })
+            .then(response => { console.log(response)})
+            .catch(error => { console.log("Error!") })
+        },
+
+
+        getTweets: function() {
+            var url = 'https://fake-tweets-api.herokuapp.com/tweets';
+            this.$http.get(url).then(response => {
+                this.tweets = response.body;
+                return this.tweets;
+            });
+        },
+
+
+        getTweetsByAuthor: function () {
+            this.$http.get('https://fake-tweets-api.herokuapp.com/tweets?author=' + this.author, {
+                author: this.author
+            })
+                .then(response => {
+                    this.tweets = response.body;
+                    console.log(response)
+                })
+                .catch(error => { console.log("Error!") })
+        },
+
+
+        getTweetsByWord: function() {
+            var url = 'https://fake-tweets-api.herokuapp.com/tweets?word=' + this.message;
+            this.$http.get(url).then(response => {
+                this.message = response.body;
+                return this.message;
+            });
+        },
+
+
+        TweetsById: function() {
+            var url = 'https://fake-tweets-api.herokuapp.com/tweets' + this.idTweets;
+            this.$http.get(url).then(response => {
+                this.tweet=response.body
+            })
+            .catch(error => { console.log("Error!") })
+        },
+
+
+        deleteTweetsById: function() {
+            var url = 'https://fake-tweets-api.herokuapp.com/tweets' + this.idTweets;
+            this.$http.delete(url).then(response => {
+                this.tweets = response.body;
+                return this.tweets;
+            })
+        }
+    },
+
+    created: function() {
+        this.getTweets();
+    }
+})
